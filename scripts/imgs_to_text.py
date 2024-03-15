f13d271c49519aecf730b791e1857b8ac9b712f1
Nargizi <ggoge19@freeuni.edu.ge>
2024-03-15 16:29:00 +0400

    add text processing

--- scripts/imgs_to_text.py
+++ scripts/imgs_to_text.py
@@ -1,8 +1,3 @@
-# import os
-# from typing import List
-# import pytesseract
-
-# import cv2
 
 import argparse
 from pathlib import Path 
@@ -10,6 +5,7 @@ from typing import List
 
 from oppy.utils.io import get_fs
 from oppy.img.process import Image
+from oppy.text.process import Texts, SplitWordProcessor, FrakturProcessor
 from oppy.ocr.tesseract import ocr
 
 
@@ -18,19 +14,22 @@ def extract_text_from_image(image_path, lang) -> str:
     print(image_path)
     image = Image.read_image(image_path)
     (image.to_grayscale()
-     .denoise_adaptive())
+     .denoise())
     return ocr(image, lang)
 
 
 
-def extract_text(args) -> List[str]:
+def extract_text(args) -> Texts:
     fs = get_fs(args.images_dir)
-    for pdf_dir in fs.list_dirs():
-        for image_path in sorted(fs.list_files(pdf_dir, args.format), 
-                            key=lambda file_name: int(Path(file_name).stem[len("img"):])):
-            print(extract_text_from_image(image_path, args.language))
-            break
-        break
+    doc = Texts()
+    doc.add_processor(SplitWordProcessor())
+    if args.language == 'frk':
+        doc.add_processor(FrakturProcessor())
+    for image_path in sorted(fs.list_files(ext=args.format), 
+                             key=lambda file_name: int(Path(file_name).stem[len("img"):]))[:20]:
+        doc.texts.append(extract_text_from_image(image_path, args.language))
+    return doc
+
     
 
 def main() -> None:
@@ -50,9 +49,12 @@ def main() -> None:
     args = parser.parse_args()
 
     texts = extract_text(args)
-    # with open(args.output_file, "w") as f:
-    #     for text in texts:
-    #         f.write(text)
+    fs_out = get_fs(args.output_file)
+    file_name = Path(args.images_dir).stem + ".txt"
+    fs_out.touch(file_name)
+    with fs_out.open(file_name, "w") as f:
+        f.write(texts.process())
+
 
 
 if __name__ == "__main__":
